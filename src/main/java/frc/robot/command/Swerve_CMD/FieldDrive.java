16ef11e10c2dd9b5c9459646d6bc18630ebcffa4
Allen95Wei <allen95wei@gmail.com>
2025-03-14 10:26:25 +0800

    test: 測試切換 Robot/Field Relative 操控方式於 FieldDrive.java 中

--- src/main/java/frc/robot/command/Swerve_CMD/FieldDrive.java
+++ src/main/java/frc/robot/command/Swerve_CMD/FieldDrive.java
@@ -4,6 +4,7 @@
 
 package frc.robot.command.Swerve_CMD;
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.generated.TunerConstants;
@@ -12,6 +13,7 @@ import frc.robot.subsystems.Elevator;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
@@ -23,24 +25,29 @@ public class FieldDrive extends Command {
   private final CommandSwerveDrivetrain swerve;
   private final Elevator elevator;
 
-  private SwerveRequest.FieldCentric drive;
+  private SwerveRequest.FieldCentric driveF;
+  private SwerveRequest.RobotCentric driveR;
 
-  private double ChassisSpeed;
+  private double rate;
+  private boolean isRobotRelative;
 
-  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+  private double maxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+  private double maxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
   private DoubleSupplier vX, vY, vR;
+  private BooleanSupplier btnIsPressed;
+
 
   /** Creates a new ChassisSpeed. */
-  public FieldDrive(CommandSwerveDrivetrain swerve, Elevator elevator, PS5Controller Driver_Ctrl) {
+  public FieldDrive(CommandSwerveDrivetrain swerve, Elevator elevator, PS5Controller driveCtrl, GenericHID switchCtrl) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.swerve = swerve;
     this.elevator = elevator;
 
-    vX = () -> -Driver_Ctrl.getLeftY();
-    vY = () -> -Driver_Ctrl.getLeftX();
-    vR = () -> -Driver_Ctrl.getRightX();
+    vX = () -> -driveCtrl.getLeftY();
+    vY = () -> -driveCtrl.getLeftX();
+    vR = () -> -driveCtrl.getRightX();
+    btnIsPressed = () -> switchCtrl.getRawButtonPressed(2);  // TODO: confirm the button's ID
 
     addRequirements(swerve, elevator);
   }
@@ -50,25 +57,43 @@ public class FieldDrive extends Command {
   public void initialize() {
     System.out.println("FieldDrive init");
     /* Setting up bindings for necessary control of the swerve drive platform */
-    drive = new SwerveRequest.FieldCentric()
-                             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-                             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    driveF = new SwerveRequest.FieldCentric()
+                              .withDeadband(maxSpeed * 0.1).withRotationalDeadband(maxAngularRate * 0.1) // Add a 10% deadband
+                              .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    driveR = new SwerveRequest.RobotCentric()
+                              .withDeadband(maxSpeed * 0.1).withRotationalDeadband(maxAngularRate * 0.1)
+                              .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // System.out.printf("%.2f %.2f %.2f\n", vX.getAsDouble(), vY.getAsDouble(), vR.getAsDouble());
-    if (elevator.getAbsolutePosition() > -35) {
-      ChassisSpeed = 1;
+    if (btnIsPressed.getAsBoolean()) {
+      isRobotRelative = true;
     }
-    else {
-      ChassisSpeed = 0.5;
+    
+    if (isRobotRelative) {  // Robot Relative
+      if (Math.abs(vR.getAsDouble()) >= 0.2) {
+        isRobotRelative = false;
+        return;
+      }
+      swerve.setControl(driveR.withVelocityX(vX.getAsDouble() * maxSpeed) // Drive forward with negative Y(forward)
+                              .withVelocityY(vY.getAsDouble() * maxSpeed) // Drive left with negative X (left)
+                              .withRotationalRate(vR.getAsDouble() * maxAngularRate * 4) // Drive counterclockwise with negative X (left)
+      );
+    } else {  // Field Relative
+      // System.out.printf("%.2f %.2f %.2f\n", vX.getAsDouble(), vY.getAsDouble(), vR.getAsDouble());
+      if (elevator.getAbsolutePosition() > -35) {
+        rate = 1;
+      }
+      else {
+        rate = 0.5;
+      }
+      swerve.setControl(driveF.withVelocityX(vX.getAsDouble() * maxSpeed * rate) // Drive forward with negative Y(forward)
+                            .withVelocityY(vY.getAsDouble() * maxSpeed * rate) // Drive left with negative X (left)
+                            .withRotationalRate(vR.getAsDouble() * maxAngularRate * 4 * rate) // Drive counterclockwise with negative X (left)
+      );
     }
-    swerve.setControl(drive.withVelocityX(vX.getAsDouble() * MaxSpeed * ChassisSpeed) // Drive forward with negative Y(forward)
-                           .withVelocityY(vY.getAsDouble() * MaxSpeed * ChassisSpeed) // Drive left with negative X (left)
-                           .withRotationalRate(vR.getAsDouble() * MaxAngularRate * 4 * ChassisSpeed) // Drive counterclockwise with negative X (left)
-    );
   }
 
   // Called once the command ends or is interrupted.
